5c2b6d07b9b37ab3e8719b90efc57702680f6caa
MSe1969 <mse1969@posteo.de>
2019-03-23 15:41:52 +0100

    switch-script: Track own Dialer and sepolicy
    Change-Id: I4ef51b85c6894d7137626afc0861cd7f00c119f0

--- switch_microG.sh
+++ switch_microG.sh
@@ -46,16 +46,14 @@ switch_branches $BRANCH1 frameworks/base
 switch_branches $BRANCH1 frameworks/native
 #switch_branches $BRANCH1 kernel/oneplus/msm8996/
 #switch_branches $BRANCH1 packages/apps/Camera2
-#switch_branches $BRANCH1 packages/apps/Dialer
+switch_branches $BRANCH1 packages/apps/Dialer
 #switch_branches $BRANCH2 packages/apps/Eleven
 #switch_branches $BRANCH2 packages/apps/LockClock
 switch_branches $BRANCH1 packages/apps/Jelly
 switch_branches $BRANCH1 packages/apps/Settings
-#switch_branches $BRANCH1 packages/apps/SetupWizard
 #switch_branches $BRANCH1 packages/apps/Trebuchet
-#switch_branches $BRANCH2 system/bt
-#switch_branches $BRANCH1 system/core
-#switch_branches $BRANCH1 system/sepolicy
+switch_branches $BRANCH1 system/core
+switch_branches $BRANCH1 system/sepolicy
 switch_branches $BRANCH1 vendor/lineage
 switch_branches $BRANCH1 .repo/local_manifests
 

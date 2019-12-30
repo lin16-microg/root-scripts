fb231a7a0d47fc0a9ed0f63c514fc4548fd2e3e8
MSe1969 <mse1969@posteo.de>
2019-12-30 23:23:24 +0100

    Track own Camera2
    Change-Id: I4c6e0b11c54a83b8e1e491a0f084b0a568abfd9d

--- switch_microG.sh
+++ switch_microG.sh
@@ -45,7 +45,7 @@ switch_zpatch $BRANCH1 R
 switch_branches $BRANCH1 frameworks/base
 switch_branches $BRANCH1 frameworks/native
 #switch_branches $BRANCH1 kernel/oneplus/msm8996/
-#switch_branches $BRANCH1 packages/apps/Camera2
+switch_branches $BRANCH1 packages/apps/Camera2
 switch_branches $BRANCH1 packages/apps/Dialer
 #switch_branches $BRANCH2 packages/apps/Eleven
 #switch_branches $BRANCH2 packages/apps/LockClock

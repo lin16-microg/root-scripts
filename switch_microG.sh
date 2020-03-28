e993f4859df701467b1ec1d56809719b24786929
MSe1969 <mse1969@posteo.de>
2020-03-28 12:57:01 +0100

    Build and switch script: Add Treble option
    Change-Id: I4df7bde81223369767f24dce08d6303079d7129b

--- switch_microG.sh
+++ switch_microG.sh
@@ -25,35 +25,64 @@ switch_zpatch() {
 }
 
 case "$1" in
+  treble)
+    BRANCH1="lin-16.0-treble"
+    BRANCH2="lin-16.0-treble"
+    BRANCH3="lin-16.0-microG"
+    PATCHV="S"
+    ;;
   microG) 
     BRANCH1="lin-16.0-microG"
+    BRANCH2="lineage-16.0"
+    BRANCH3="lin-16.0-microG"
+    PATCHV="S"
     ;;
   default) 
     BRANCH1="lineage-16.0"
+    BRANCH2="lineage-16.0"
+    BRANCH3="lineage-16.0"
+    PATCHV="S"
+    ;;
+  reference) 
+    BRANCH1="lineage-16.0"
+    BRANCH2="lineage-16.0"
+    BRANCH3="lineage-16.0"
+    PATCHV="N"
     ;;
   *) 
-    echo "usage: switch_microg.sh default | microG"
+    echo "usage: switch_microg.sh default | microG | treble | reference"
     echo "-"
     echo "  default   - LineageOS 16.0"
     echo "  microG    - hardened microG build"
+    echo "  treble    - Treble GSI build"
+    echo "  reference - 100% LineageOS 15.1 (no patches - for 'repo sync')"
     exit
     ;;   
 esac
 
 switch_zpatch $BRANCH1 R
 
+switch_branches $BRANCH1 build/make
+switch_branches $BRANCH2 external/selinux
+switch_branches $BRANCH2 frameworks/av
 switch_branches $BRANCH1 frameworks/base
 switch_branches $BRANCH1 frameworks/native
-switch_branches $BRANCH1 packages/apps/Camera2
-switch_branches $BRANCH1 packages/apps/Dialer
-switch_branches $BRANCH1 packages/apps/Jelly
+switch_branches $BRANCH2 frameworks/opt/net/wifi
+switch_branches $BRANCH2 frameworks/opt/telephony
+switch_branches $BRANCH3 packages/apps/Camera2
+switch_branches $BRANCH3 packages/apps/Dialer
+switch_branches $BRANCH3 packages/apps/Jelly
 switch_branches $BRANCH1 packages/apps/LineageParts
 switch_branches $BRANCH1 packages/apps/Settings
-#switch_branches $BRANCH1 packages/apps/Trebuchet
+switch_branches $BRANCH2 packages/services/Telephony
+switch_branches $BRANCH2 system/bt
 switch_branches $BRANCH1 system/core
+switch_branches $BRANCH2 system/netd
 switch_branches $BRANCH1 system/sepolicy
+switch_branches $BRANCH2 system/vold
 switch_branches $BRANCH1 vendor/lineage
+switch_branches $BRANCH2 vendor/qcom/opensource/cryptfs_hw
 switch_branches $BRANCH1 .repo/local_manifests
 
-switch_zpatch $BRANCH1 S
+switch_zpatch $BRANCH1 $PATCHV
 

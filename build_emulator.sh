7247eae31a9dd4c6065a55363374a6ceb58eb7bc
MSe1969 <mse1969@posteo.de>
2020-09-07 09:51:01 +0200

    Adapt build script for emulator, add start script
    Change-Id: I3764cc49fba01a123b7c09576d65f817953dd575

--- build_emulator.sh
+++ build_emulator.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # Own values
-export CCACHE_DIR=~/android/.ccache16
+export CCACHE_DIR=~/.ccache16
+export OUT_DIR_COMMON_BASE=~/out-android
 
 # Use pre-defined build script
-source z_patches/build_device.sh x86 test
+source z_patches/build_device.sh x86 $1 $2
 

7ec033c68cedefd976ea2ed675b25a0750b06bec
MSe1969 <mse1969@posteo.de>
2021-01-08 11:03:49 +0100

    Build-scripts: 1. Add gts210vewifi / 2. move ccache location
    Change-Id: I1e6568ceeea73a63d66af10c6942cb22cd5f0dcc

--- build_gts210vewifi.sh
+++ build_gts210vewifi.sh
@@ -5,7 +5,7 @@ export CCACHE_DIR=~/android/.ccache16
 export OUT_DIR_COMMON_BASE=~/out-android
 
 # Use pre-defined build script
-source z_patches/build_treble.sh $1 $2 $3
+source z_patches/build_device.sh gts210vewifi $1
 
 
 

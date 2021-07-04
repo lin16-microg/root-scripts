b5c09577bc91b186becc87dd9ed0ba2e0d9aeee0
MSe1969 <mse1969@posteo.de>
2021-07-04 19:44:37 +0200

    Adapt build-scripts to new server structure
    Change-Id: I0cbf31287c551d7943933dbca275996b4a354a09

--- build_gts210vewifi.sh
+++ build_gts210vewifi.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Own values
-export CCACHE_DIR=~/android/.ccache16
+export CCACHE_DIR=~/out-android/.ccache
 export OUT_DIR_COMMON_BASE=~/out-android
 
 # Use pre-defined build script
